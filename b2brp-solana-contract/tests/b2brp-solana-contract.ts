289fb97e3068c9f9e8fb534da8efe9e75552bae0
mucks <mail@mucks.dev>
2023-03-06 12:59:44 +0700

    added example.json to test contract nft

--- b2brp-solana-contract/tests/b2brp-solana-contract.ts
+++ b2brp-solana-contract/tests/b2brp-solana-contract.ts
@@ -3,7 +3,13 @@ import { Program } from "@project-serum/anchor";
 import { B2brpSolanaContract } from "../target/types/b2brp_solana_contract";
 
 describe("b2brp-solana-contract", () => {
-  // Configure the client to use the local cluster.
+
+  const testTitle = "B2BRP Solana Contract";
+  const testNftSymbol = "B2BRP";
+  const tetNftUri = "https://b2brp.com";
+
+
+
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.B2brpSolanaContract as Program<B2brpSolanaContract>;
